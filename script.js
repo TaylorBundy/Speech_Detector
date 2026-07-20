442f7a5034e828ade27008dad4e5dbfe90e988ce
TaylorBundy <martonbarbosa@hotmail.com>
2026-07-20 02:01:08 -0300

    casi

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ const idioma = document.getElementById("idioma");
 const original = document.getElementById("original");
 const traducido = document.getElementById("traducido");
 let escuchando = false;
+const palabraActivacion = "escuchar";
+const palabraDetener = "para";
+const palabraDetenerFinal = "finalizar";
+let activo = true;
+let temporizador = null;
+const radios = document.querySelector(".radio");
+const radioEspañol = document.querySelector("#Español");
+const radioPortugues = document.querySelector("#Portugues");
+
+document.addEventListener("DOMContentLoaded", () => {
+  temporizador = setTimeout(() => {
+    if (radioPortugues.checked) {
+      reconocimiento.lang = "pt-BR";
+    }
+    if (radioEspañol.checked) {
+      reconocimiento.lang = "es-ES";
+    }
+    if (radioEspañol.checked || radioPortugues.checked) {
+      activo = true;
+      iniciarMicrofono();
+    }
+  }, 10000);
+});
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -18,12 +41,18 @@ reconocimiento.interimResults = false;
 // reconocimiento.interimResults = true;
 
 // escucha directamente portugués BR
-reconocimiento.lang = "pt-BR";
+// if (radioPortugues.checked) {
+//   reconocimiento.lang = "pt-BR";
+// }
+// if (radioEspañol.checked) {
+//   reconocimiento.lang = "es-ES";
+// }
 
 reconocimiento.onstart = () => {
   microfonoActivo = true;
 
   estado.innerHTML = "🟢 Escuchando...";
+  estado.title = "El micrófono esta activo y escuchando.";
 
   estadoMic.innerHTML = "🟢 Micrófono activado";
   estadoMic.className = "encendido";
@@ -51,15 +80,18 @@ reconocimiento.onend = () => {
   estadoMic.className = "apagado";
 
   boton.innerHTML = "🎤 Activar micrófono";
+  if (activo) {
+    iniciarMicrofono();
+  }
 
   // Si el usuario no lo apagó, volver a escuchar
-  if (escuchando) {
-    setTimeout(() => {
-      try {
-        reconocimiento.start();
-      } catch {}
-    }, 300);
-  }
+  //   if (escuchando) {
+  //     setTimeout(() => {
+  //       try {
+  //         reconocimiento.start();
+  //       } catch {}
+  //     }, 300);
+  //   }
 };
 
 // reconocimiento.onresult = async (e) => {
@@ -76,18 +108,39 @@ reconocimiento.onresult = async (e) => {
   let resultado = e.results[e.results.length - 1];
   console.log("Resultado:", resultado);
 
-  let texto = resultado[0].transcript;
+  let texto = resultado[0].transcript.toLowerCase().trim();
+  if (activo) {
+    if (texto.includes(palabraDetener)) {
+      detenerMicrofono();
+      return;
+    } else if (texto.includes(palabraDetenerFinal)) {
+      detenerMicrofonoCompleto();
+      return;
+    }
+  } else {
+    if (texto.includes(palabraActivacion)) {
+      activo = true;
+      console.log("Activado nuevamente");
+    }
+  }
   const idioma2 = await detectarIdioma(texto);
-  console.log("Idioma detectado:", idioma2);
+  if (idioma2 !== "PT") {
+    //console.log("Idioma detectado:", idioma2);
+    idioma.textContent = `Idioma detectado: ${idioma2}`;
+    original.textContent = texto;
+    traducido.textContent = "No se traducirá porque no es portugués.";
+    return; // No traducir si no es portugués
+  } else {
+    //console.log("Idioma detectado:", idioma2);
+    original.textContent = texto;
 
-  original.textContent = texto;
+    // Solo traducir cuando la frase terminó
+    //if (!resultado.isFinal) return;
 
-  // Solo traducir cuando la frase terminó
-  if (!resultado.isFinal) return;
+    idioma.textContent = "Portugués (Brasil)";
 
-  idioma.textContent = "Portugués (Brasil)";
-
-  traducir(texto);
+    traducir(texto);
+  }
 };
 
 async function traducir(texto) {
@@ -129,8 +182,56 @@ async function detectarIdioma(texto) {
   });
 
   const datos = await respuesta.json();
+  console.log("Idioma detectado:", datos.idioma);
+  console.log(datos);
 
   return datos.idioma;
+}
+
+function iniciarMicrofono() {
+  try {
+    reconocimiento.start();
+    console.log("Micrófono iniciado");
+  } catch (e) {}
+}
+
+function detenerMicrofono() {
+  activo = false;
+  reconocimiento.stop();
+  idioma.textContent = "";
+  original.textContent = "";
+  traducido.textContent = "";
+  console.log("Micrófono detenido");
+  estado.title = "El micrófono se ha detenido. Puede activarlo nuevamente.";
+
+  // Reactivar automáticamente después de 10 segundos
+  temporizador = setTimeout(() => {
+    activo = true;
+    iniciarMicrofono();
+  }, 10000);
+}
+
+function detenerMicrofonoCompleto() {
+  activo = false;
+  reconocimiento.stop();
+  idioma.textContent = "";
+  original.textContent = "";
+  traducido.textContent = "";
+  if (radioEspañol.checked) {
+    radioEspañol.checked = false;
+  }
+  if (radioPortugues.checked) {
+    radioPortugues.checked = false;
+  }
+  console.log("Micrófono detenido completo");
+  estado.title =
+    "El micrófono se ha detenido completamente. Puede activarlo nuevamente.";
+
+  //   // Reactivar automáticamente después de 10 segundos
+  //   temporizador = setTimeout(() => {
+  //     activo = true;
+  //     iniciarMicrofono();
+  //   }, 10000);
 }
 
 // boton.onclick = () => {
@@ -142,11 +243,13 @@ async function detectarIdioma(texto) {
 // };
 
 boton.onclick = () => {
-  escuchando = !escuchando;
+  if (radioEspañol.checked === true || radioPortugues.checked === true) {
+    escuchando = !escuchando;
 
-  if (escuchando) {
-    reconocimiento.start();
-  } else {
-    reconocimiento.stop();
+    if (escuchando) {
+      reconocimiento.start();
+    } else {
+      reconocimiento.stop();
+    }
   }
 };
